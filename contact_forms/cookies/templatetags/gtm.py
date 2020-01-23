036dd9add8b088bc78c1cc5c6eea33d564171fc1
Mark Higham <mark.higham@gmail.com>
2020-01-23 15:31:31 +0000

    Cookie banner (#10)
    * adding framework for cookie banner
    
    * copied cookie policy from helpdesk
    
    * adding gtm tags to base.html and using GTM_CONTAINER_ID from env
    variables
    
    * changed button width on desktops
    
    * removing the customisation of hint colour
    
    * width of buttons in cookie banner on desktops
    
    * defaulting to all cookies setting to false
    
    * removed .editorconfig from git and ignored for future dev
    
    * updating to use govuk-frontend v3
    
    * removing irrelevant comments

---
+++ contact_forms/cookies/templatetags/gtm.py
@@ -0,0 +1,33 @@
+from . import register
+from django.utils.safestring import mark_safe
+from django.conf import settings
+from django.template import loader
+
+
+@register.inclusion_tag("banner.html")
+def cookie_banner():
+    return {}
+
+
+def render_gtm_template(template_filename, gtm_container_id):
+    t = loader.get_template(template_filename)
+
+    return t.render({
+        'GTM_CONTAINER_ID': gtm_container_id
+    })
+
+
+@register.simple_tag()
+def google_tag_manager():
+    if not settings.IEE_GA_GTM:
+        return mark_safe("<!-- missing GTM container id -->")
+
+    return render_gtm_template("gtm.html", settings.IEE_GA_GTM)
+
+
+@register.simple_tag()
+def google_tag_manager_noscript():
+    if not settings.IEE_GA_GTM:
+        return mark_safe("<!-- missing GTM container id -->")
+
+    return render_gtm_template("gtm_noscript.html",settings.IEE_GA_GTM)
