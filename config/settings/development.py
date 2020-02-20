232b2d7138414517f362d316981d7894ce00375d
Glen Lavery <glavery@users.noreply.github.com>
2020-02-20 12:57:51 +0000

    Add django environ (#33)
    * readme
    
    * swtiched to using djanog environ
    
    * fixed requirements entry
    
    * added test_db_passwrd for docker tests
    
    * added trust env for docker db
    
    * reinstate README.md contents
    
    * removed print statement
    
    * fixed a couple of minor settings issues sand updated envs templates

--- config/settings/development.py
+++ config/settings/development.py
@@ -20,11 +20,11 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
 DATABASES = {
     "default": {
         "ENGINE": "psqlextra.backend",  # 'django.db.backends.postgresql_psycopg2',
-        "NAME": os.environ.get("DJANGO_POSTGRES_DATABASE"),
-        "USER": os.environ.get("DJANGO_POSTGRES_USER"),
-        "PASSWORD": os.environ.get("DJANGO_POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("DJANGO_POSTGRES_HOST"),
-        "PORT": os.environ.get("DJANGO_POSTGRES_PORT"),
+        "NAME": env.str("DJANGO_POSTGRES_DATABASE"),
+        "USER": env.str("DJANGO_POSTGRES_USER"),
+        "PASSWORD": env.str("DJANGO_POSTGRES_PASSWORD"),
+        "HOST": env.str("DJANGO_POSTGRES_HOST"),
+        "PORT": env.str("DJANGO_POSTGRES_PORT"),
     }
 }
 
@@ -36,7 +36,7 @@ SESSION_COOKIE_SECURE = False
 SESSION_COOKIE_HTTPONLY = False
 CSRF_COOKIE_SECURE = False
 
-STATIC_ROOT = "contact_form/static"
+STATIC_ROOT = "contact_forms/static"
 INTERNAL_IPS = ["127.0.0.1", "0.0.0.0", "localhost"]
 
 INSTALLED_APPS += ["debug_toolbar"]
@@ -57,4 +57,4 @@ ELASTICSEARCH_DSL = {"default": {"hosts": ES_URL}}
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR + "/app-messages"
 
-FEEDBACK_DESTINATION_EMAIL = os.environ.get("FEEDBACK_DESTINATION_EMAIL")
+FEEDBACK_DESTINATION_EMAIL = env.str("FEEDBACK_DESTINATION_EMAIL")
--- config/settings/test.py
+++ config/settings/test.py
@@ -20,11 +20,11 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
 DATABASES = {
     "default": {
         "ENGINE": "psqlextra.backend",  # 'django.db.backends.postgresql_psycopg2',
-        "NAME": os.environ.get("DJANGO_POSTGRES_DATABASE"),
-        "USER": os.environ.get("DJANGO_POSTGRES_USER"),
-        "PASSWORD": os.environ.get("DJANGO_POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("DJANGO_POSTGRES_HOST"),
-        "PORT": os.environ.get("DJANGO_POSTGRES_PORT"),
+        "NAME": env.str("DJANGO_POSTGRES_DATABASE"),
+        "USER": env.str("DJANGO_POSTGRES_USER"),
+        "PASSWORD": env.str("DJANGO_POSTGRES_PASSWORD"),
+        "HOST": env.str("DJANGO_POSTGRES_HOST"),
+        "PORT": env.str("DJANGO_POSTGRES_PORT"),
     }
 }
 
@@ -36,19 +36,7 @@ SESSION_COOKIE_SECURE = False
 SESSION_COOKIE_HTTPONLY = False
 CSRF_COOKIE_SECURE = False
 
-STATIC_ROOT = "contact_form/static"
-INTERNAL_IPS = ["127.0.0.1", "0.0.0.0", "localhost"]
-
-INSTALLED_APPS += ["debug_toolbar"]
-
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-
-
-def show_toolbar(request):
-    return True
-
-
-DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": show_toolbar}
+STATIC_ROOT = "contact_forms/static"
 
 ES_URL = "http://es:9200"
 
@@ -57,12 +45,12 @@ ELASTICSEARCH_DSL = {"default": {"hosts": ES_URL}}
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR + "/app-messages"
 
-FEEDBACK_DESTINATION_EMAIL = os.environ.get("FEEDBACK_DESTINATION_EMAIL")
+FEEDBACK_DESTINATION_EMAIL = env.str("FEEDBACK_DESTINATION_EMAIL")
 
 
 INSTALLED_APPS.append("django_nose")
 TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
-TEST_OUTPUT_DIR = os.environ.get("TEST_OUTPUT_DIR", ".")
+TEST_OUTPUT_DIR = env.str("TEST_OUTPUT_DIR")
 NOSE_ARGS = [
     "--verbosity=3",
     "--nologcapture",
