3dfb761064e53817af8377de6d0a53636b848e02
Kevin Carrogan <kevin.carrogan@gmail.com>
2021-05-28 15:01:23 +0100

    Add prettier pre-commit hook and run

--- assets/javascript/global.js
+++ assets/javascript/global.js
@@ -1,19 +1,19 @@
-var Button = require('govuk-frontend/govuk/components/button/button.js')
-var ErrorSummary = require('govuk-frontend/govuk/components/error-summary/error-summary.js')
-var CookiePolicy = require('./modules/cookie-policy');
+var Button = require("govuk-frontend/govuk/components/button/button.js");
+var ErrorSummary = require("govuk-frontend/govuk/components/error-summary/error-summary.js");
+var CookiePolicy = require("./modules/cookie-policy");
 
 var cookiePolicy = new CookiePolicy();
-cookiePolicy.initBanner('.app-cookie-banner', '.js-accept-cookie', 'cookies');
+cookiePolicy.initBanner(".app-cookie-banner", ".js-accept-cookie", "cookies");
 
 // accessibility feature
-new Button(document).init()
+new Button(document).init();
 
 // error summary focus on page load
-var $errorSummary = document.querySelector('[data-module="error-summary"]')
+var $errorSummary = document.querySelector('[data-module="error-summary"]');
 if ($errorSummary) {
-    new ErrorSummary($errorSummary).init()
+  new ErrorSummary($errorSummary).init();
 }
 
 module.exports = {
-    bindCookiePolicyForm: cookiePolicy.bindForm
-}
+  bindCookiePolicyForm: cookiePolicy.bindForm,
+};
--- assets/javascript/modules/cookie-policy.js
+++ assets/javascript/modules/cookie-policy.js
@@ -1,41 +1,41 @@
-'use strict'
+"use strict";
 
 function CookieBanner() {
-  var cookiePreferencesName = 'cookie_preferences_set'
-  var cookiePreferencesDurationDays = 31
+  var cookiePreferencesName = "cookie_preferences_set";
+  var cookiePreferencesDurationDays = 31;
 
-  var cookiesPolicyName = 'cookies_policy'
-  var cookiesPolicyDurationDays = 365
+  var cookiesPolicyName = "cookies_policy";
+  var cookiesPolicyDurationDays = 365;
 
   function setCookie(name, value, options) {
-    if (typeof options === 'undefined') {
-      options = {}
+    if (typeof options === "undefined") {
+      options = {};
     }
-    var cookieString = name + '=' + value + '; path=/'
+    var cookieString = name + "=" + value + "; path=/";
     if (options.days) {
-      var date = new Date()
-      date.setTime(date.getTime() + options.days * 24 * 60 * 60 * 1000)
-      cookieString = cookieString + '; expires=' + date.toGMTString()
+      var date = new Date();
+      date.setTime(date.getTime() + options.days * 24 * 60 * 60 * 1000);
+      cookieString = cookieString + "; expires=" + date.toGMTString();
     }
-    if (document.location.protocol === 'https:') {
-      cookieString = cookieString + '; Secure'
+    if (document.location.protocol === "https:") {
+      cookieString = cookieString + "; Secure";
     }
-    document.cookie = cookieString
+    document.cookie = cookieString;
   }
 
   function getCookie(name) {
-    var nameEQ = name + '='
-    var cookies = document.cookie.split(';')
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(";");
     for (var i = 0, len = cookies.length; i < len; i++) {
-      var cookie = cookies[i]
-      while (cookie.charAt(0) === ' ') {
-        cookie = cookie.substring(1, cookie.length)
+      var cookie = cookies[i];
+      while (cookie.charAt(0) === " ") {
+        cookie = cookie.substring(1, cookie.length);
       }
       if (cookie.indexOf(nameEQ) === 0) {
-        return decodeURIComponent(cookie.substring(nameEQ.length))
+        return decodeURIComponent(cookie.substring(nameEQ.length));
       }
     }
-    return null
+    return null;
   }
 
   function getDefaultPolicy() {
@@ -43,156 +43,166 @@ function CookieBanner() {
       essential: true,
       settings: false,
       usage: false,
-      campaigns: false
-    }
+      campaigns: false,
+    };
   }
 
   function getPolicyOrDefault() {
-    var cookie = getCookie(cookiesPolicyName)
-    var policy = getDefaultPolicy()
-    if (!cookie) return policy
+    var cookie = getCookie(cookiesPolicyName);
+    var policy = getDefaultPolicy();
+    if (!cookie) return policy;
 
     try {
-      var parsed = JSON.parse(cookie)
+      var parsed = JSON.parse(cookie);
 
-      policy.campaigns = parsed.campaigns || false
-      policy.usage = parsed.usage || false
-      policy.settings = parsed.settings || false
-
+      policy.campaigns = parsed.campaigns || false;
+      policy.usage = parsed.usage || false;
+      policy.settings = parsed.settings || false;
     } catch (e) {
-      return policy
+      return policy;
     }
 
-    return policy
-
+    return policy;
   }
 
   function createPoliciesCookie(settings, usage, campaigns) {
-    var policy = getDefaultPolicy()
-    policy.settings = settings || false
-    policy.usage = usage || false
-    policy.campaigns = campaigns || false
-    var json = JSON.stringify(policy)
-    setCookie(cookiesPolicyName, json, {days: cookiesPolicyDurationDays})
-    return policy
+    var policy = getDefaultPolicy();
+    policy.settings = settings || false;
+    policy.usage = usage || false;
+    policy.campaigns = campaigns || false;
+    var json = JSON.stringify(policy);
+    setCookie(cookiesPolicyName, json, { days: cookiesPolicyDurationDays });
+    return policy;
   }
 
   function hideCookieBanner(className) {
-    var banner = document.querySelector(className)
-    banner.className = banner.className.replace(/app-cookie-banner--show/, '')
+    var banner = document.querySelector(className);
+    banner.className = banner.className.replace(/app-cookie-banner--show/, "");
   }
 
   function displayCookieBannerAcceptAll(cookieBannerClassName) {
-    var banner = document.querySelector(cookieBannerClassName)
-    banner.className = banner.className + ' app-cookie-banner--show__accepted-all'
+    var banner = document.querySelector(cookieBannerClassName);
+    banner.className =
+      banner.className + " app-cookie-banner--show__accepted-all";
 
-    var hideButton = document.querySelector('.hide-button')
+    var hideButton = document.querySelector(".hide-button");
     if (hideButton.attachEvent) {
-      hideButton.attachEvent('click', function () {
-        hideCookieBanner(cookieBannerClassName)
-      })
+      hideButton.attachEvent("click", function () {
+        hideCookieBanner(cookieBannerClassName);
+      });
     } else {
-      hideButton.addEventListener('click', function (e) {
-        hideCookieBanner(cookieBannerClassName)
-        e.preventDefault()
-        return false;
-      }, false)
+      hideButton.addEventListener(
+        "click",
+        function (e) {
+          hideCookieBanner(cookieBannerClassName);
+          e.preventDefault();
+          return false;
+        },
+        false
+      );
     }
-
   }
 
   function displayCookieBanner(className) {
-    var banner = document.querySelector(className)
+    var banner = document.querySelector(className);
 
     banner.className = banner.className.replace(
       /js--cookie-banner/,
-      'app-cookie-banner--show'
-    )
+      "app-cookie-banner--show"
+    );
   }
 
   function bindAcceptAllCookiesButton(className, onClickFunction) {
-    var button = document.querySelector(className)
+    var button = document.querySelector(className);
     if (button.attachEvent) {
-      button.attachEvent('onclick', onClickFunction)
+      button.attachEvent("onclick", onClickFunction);
     } else {
-      button.addEventListener('click', onClickFunction, false)
+      button.addEventListener("click", onClickFunction, false);
     }
   }
 
   function setPreferencesCookie() {
-    setCookie(cookiePreferencesName, 'true', {days: cookiePreferencesDurationDays})
+    setCookie(cookiePreferencesName, "true", {
+      days: cookiePreferencesDurationDays,
+    });
   }
 
   function enableCookieBanner(bannerClassName, acceptButtonClassName) {
-    displayCookieBanner(bannerClassName)
+    displayCookieBanner(bannerClassName);
     bindAcceptAllCookiesButton(acceptButtonClassName, function () {
-      createPoliciesCookie(true, true, true)
+      createPoliciesCookie(true, true, true);
 
-      setPreferencesCookie()
-      displayCookieBannerAcceptAll(bannerClassName)
+      setPreferencesCookie();
+      displayCookieBannerAcceptAll(bannerClassName);
 
-      return false
-    })
-
+      return false;
+    });
   }
 
   function init(bannerClassName, acceptButtonClassName, cookiesPolicyUrl) {
     if (!bannerClassName) {
-      throw 'Expected bannerClassName'
+      throw "Expected bannerClassName";
     }
 
-    var preferenceCookie = getCookie(cookiePreferencesName)
-    var isCookiesPage = document.URL.indexOf(cookiesPolicyUrl) !== -1
+    var preferenceCookie = getCookie(cookiePreferencesName);
+    var isCookiesPage = document.URL.indexOf(cookiesPolicyUrl) !== -1;
 
-    if ((!preferenceCookie) && !isCookiesPage) {
-      enableCookieBanner(bannerClassName, acceptButtonClassName)
+    if (!preferenceCookie && !isCookiesPage) {
+      enableCookieBanner(bannerClassName, acceptButtonClassName);
     }
   }
 
-  function bindCookiePolicyForm(formSelector, confirmationSelector, radioButtons) {
-    if (typeof radioButtons !== 'object') {
-      throw 'expected an object with radio button selectors'
+  function bindCookiePolicyForm(
+    formSelector,
+    confirmationSelector,
+    radioButtons
+  ) {
+    if (typeof radioButtons !== "object") {
+      throw "expected an object with radio button selectors";
     }
 
-    var form = document.querySelector(formSelector)
+    var form = document.querySelector(formSelector);
 
     if (!form) {
-      throw formSelector + ' was not found'
+      throw formSelector + " was not found";
     }
 
-    var confirmation = document.querySelector(confirmationSelector)
+    var confirmation = document.querySelector(confirmationSelector);
     if (!confirmation) {
-      throw confirmationSelector + ' was not found'
+      throw confirmationSelector + " was not found";
     }
 
-    var policy = getPolicyOrDefault()
+    var policy = getPolicyOrDefault();
 
-    form[radioButtons.usage].value = policy.usage ? 'on' : 'off'
-    form[radioButtons.settings].value = policy.settings ? 'on' : 'off'
-    form[radioButtons.campaigns].value = policy.campaigns ? 'on' : 'off'
+    form[radioButtons.usage].value = policy.usage ? "on" : "off";
+    form[radioButtons.settings].value = policy.settings ? "on" : "off";
+    form[radioButtons.campaigns].value = policy.campaigns ? "on" : "off";
 
-    var attachEventMethod = form.attachEvent || form.addEventListener
-    attachEventMethod('submit', function (e) {
+    var attachEventMethod = form.attachEvent || form.addEventListener;
+    attachEventMethod(
+      "submit",
+      function (e) {
+        var settings = form[radioButtons.settings].value === "on";
+        var usage = form[radioButtons.usage].value === "on";
+        var campaigns = form[radioButtons.campaigns].value === "on";
 
-      var settings = form[radioButtons.settings].value === 'on'
-      var usage = form[radioButtons.usage].value === 'on'
-      var campaigns = form[radioButtons.campaigns].value === 'on'
+        createPoliciesCookie(settings, usage, campaigns);
+        setPreferencesCookie();
 
-      createPoliciesCookie(settings, usage, campaigns)
-      setPreferencesCookie()
+        confirmation.style = "display:block;";
+        window.scrollTo(0, 0);
 
-      confirmation.style = 'display:block;'
-      window.scrollTo(0, 0)
-
-      e.preventDefault()
-      return false
-    }, false)
+        e.preventDefault();
+        return false;
+      },
+      false
+    );
   }
 
   return {
     initBanner: init,
-    bindForm: bindCookiePolicyForm
-  }
+    bindForm: bindCookiePolicyForm,
+  };
 }
 
-module.exports = CookieBanner
+module.exports = CookieBanner;
