10ee6b69343ab322de51a4514cba8aed29e4944d
santinomolinaro <santino.molinaro@mail>
2021-06-18 11:25:38 +0100

    Moving Sentry Init to run earlier

--- assets/javascript/global.js
+++ assets/javascript/global.js
@@ -3,6 +3,15 @@ var ErrorSummary = require("govuk-frontend/govuk/components/error-summary/error-
 var CookiePolicy = require("./modules/cookie-policy");
 var Sentry = require("@sentry/browser");
 
+// include Sentry initialisation for frontend errors
+// Next comment needed to define globals for format checks
+/*global SENTRY_DSN, SENTRY_ENVIRONMENT*/
+Sentry.init({
+  dsn: SENTRY_DSN,
+  environment: SENTRY_ENVIRONMENT,
+  tracesSampleRate: 1.0,
+});
+
 var cookiePolicy = new CookiePolicy();
 cookiePolicy.initBanner(".app-cookie-banner", ".js-accept-cookie", "cookies");
 
@@ -14,16 +23,3 @@ var $errorSummary = document.querySelector('[data-module="error-summary"]');
 if ($errorSummary) {
   new ErrorSummary($errorSummary).init();
 }
-
-// include Sentry initialisation for frontend errors
-// Next comment needed to define globals for format checks
-/*global SENTRY_DSN, SENTRY_ENVIRONMENT*/
-Sentry.init({
-  dsn: SENTRY_DSN,
-  environment: SENTRY_ENVIRONMENT,
-  tracesSampleRate: 1.0,
-});
-
-module.exports = {
-  bindCookiePolicyForm: cookiePolicy.bindForm,
-};
