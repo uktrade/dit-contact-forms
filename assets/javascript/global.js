b3d31de22f07ccaee24ef69f6a782c7097b0cbde
santinomolinaro <santino.molinaro@mail>
2021-06-18 09:38:23 +0100

    logging

--- assets/javascript/global.js
+++ assets/javascript/global.js
@@ -18,6 +18,8 @@ if ($errorSummary) {
 // include Sentry initialisation for frontend errors
 // Next comment needed to define globals for format checks
 /*global SENTRY_DSN, SENTRY_ENVIRONMENT*/
+console.log(SENTRY_DSN)
+console.log(SENTRY_ENVIRONMENT)
 Sentry.init({
   dsn: SENTRY_DSN,
   environment: SENTRY_ENVIRONMENT,
