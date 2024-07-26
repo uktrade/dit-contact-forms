14a9bee428eafdd2c8b99b4e1d436f70332cc783
santinomolinaro <santino.molinaro@mail>
2024-07-26 10:45:23 +0100

    Converting to use ESS method of form tools implementation

--- ui_test/selectors/questionnaire.py
+++ ui_test/selectors/questionnaire.py
@@ -1,7 +1,7 @@
 QUESTIONNAIRE = {
     "step1": {
-        "export_from_uk": "#id_step_one-location_0",
-        "help": "#id_step_one-location_1",
+        "export_from_uk": "#id_step_one-enquiry_type_0",
+        "help": "#id_step_one-enquiry_type_1",
     },
     "step2": {
         "custom": "#id_step_two-enquiry_topic_0",
