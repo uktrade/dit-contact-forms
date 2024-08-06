216eb2f9d0e1658525973f9b46cb9053d30401c2
santinomolinaro <santino.molinaro@mail>
2024-08-06 14:59:39 +0100

    another

--- config/settings/test.py
+++ config/settings/test.py
@@ -6,28 +6,28 @@ from .base import *
 # TEMPLATES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#templates
-# TEMPLATES[0]["OPTIONS"]["debug"] = True  # noqa F405
-# TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
-#    (
-#        "django.template.loaders.cached.Loader",
-#        [
-#            "django.template.loaders.filesystem.Loader",
-#            "django.template.loaders.app_directories.Loader",
-#        ],
-#    )
-# ]
+TEMPLATES[0]["OPTIONS"]["debug"] = True  # noqa F405
+TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
+    (
+        "django.template.loaders.cached.Loader",
+        [
+            "django.template.loaders.filesystem.Loader",
+            "django.template.loaders.app_directories.Loader",
+        ],
+    )
+]
 
 # DEBUG = True
 #
 ## Secure cookie settings.
-# SESSION_COOKIE_SECURE = False
-# SESSION_COOKIE_HTTPONLY = False
-# CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+SESSION_COOKIE_HTTPONLY = False
+CSRF_COOKIE_SECURE = False
 #
-# STATIC_ROOT = "contact_forms/static"
+STATIC_ROOT = "contact_forms/static"
 #
-# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-# EMAIL_FILE_PATH = BASE_DIR + "/app-messages"
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR + "/app-messages"
 
 # INSTALLED_APPS.append("django_nose")
 # TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
